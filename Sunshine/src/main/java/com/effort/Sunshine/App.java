1ebf94cd78868a488764083848ed11c6f04bd1b6
lixun <919302586@qq.com>
2015-11-03 10:44:57 +0800

    阳光社区的最最基本框架

---
+++ Sunshine/src/main/java/com/effort/Sunshine/App.java
@@ -0,0 +1,13 @@
+package com.effort.Sunshine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
